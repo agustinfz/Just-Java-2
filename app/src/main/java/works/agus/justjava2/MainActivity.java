7aa059a2f64bf9ef75eae0eb7ca4be39df57c954
Agustin Fonts <agustinfonts@gmail.com>
2016-06-28 11:49:23 -0700

    Wiring in quantity and price display upon order button push

--- app/src/main/java/works/agus/justjava2/MainActivity.java
+++ app/src/main/java/works/agus/justjava2/MainActivity.java
@@ -2,6 +2,10 @@ package works.agus.justjava2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.text.NumberFormat;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void submitOrder (View view){
+        int quantity = 3;
+        int price = 5;
+        display(quantity);
+        displayPrice(quantity * price);
+    }
+
+    /**
+     * This method sets the given quantity value on the screen
+     * @param number
+     */
+    private void display (int number){
+        TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
+        quantityTextView.setText("" + number);
+    }
+
+    /**
+     * This method displays the price of the order using the correct currency symbol and format
+     * @param number
+     */
+
+    private void displayPrice (int number){
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
+
     }
 }
